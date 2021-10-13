13aee00d2d01297f696514f1665b1b0727f2cbbc
Richard Boháč <rici.bohac@gmail.com>
2021-10-14 01:09:07 +0200

    fix of config to function out of box

--- configs/config.php
+++ configs/config.php
@@ -3,7 +3,7 @@
 define('ABS_PATH', __DIR__ . '/../');
 
 define('DECORATOR_REGEX', '/test\.(\w+)/');
-define('COMPARATOR_STRINGS', ['debug', 'test', 123]);
+define('COMPARATOR_STRINGS', ['debug', 'test']);
 define('INPUT_FILE_PATH', ABS_PATH . 'input.txt');
 define('PRINT_AFTER_LINES', 2);
 
