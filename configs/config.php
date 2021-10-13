e7626e4f48fd81f251832ae5a0b90649e68296ce
Richard Boháč <rici.bohac@gmail.com>
2021-10-14 01:10:30 +0200

    removed duplicate file

--- configs/config.php
+++ configs/config.php
@@ -3,8 +3,8 @@
 define('ABS_PATH', __DIR__ . '/../');
 
 define('DECORATOR_REGEX', '/test\.(\w+)/');
-define('COMPARATOR_STRINGS', ['debug', 'test']);
-define('INPUT_FILE_PATH', ABS_PATH . 'input.txt');
+define('COMPARATOR_STRINGS', ['debug']);
+define('INPUT_FILE_PATH', ABS_PATH . 'example.log');
 define('PRINT_AFTER_LINES', 2);
 
 ?>
